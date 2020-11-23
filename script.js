164804bcab387527f80fa5938760b0c620a00a97
Maxim Padolski <47753504+DRN27@users.noreply.github.com>
2020-11-23 08:45:41 +0200

    FIX: crop size; bugs

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ const app = new Vue({
         croppedImgWidth: 0,
         croppedImgHeight: 0,
         croppedImgSrc: '',
-        bagetMaterial: 'Ольха'
+        bagetMaterial: 'Ольха',
+        blockInputs: false
     },
     methods: {
         uploadImg() {
@@ -35,103 +36,111 @@ const app = new Vue({
                     const croppedImg = this.$refs.croppedImg;
 
                     cropper = new Cropper(img, {
-                        aspectRatio: 4 / 3,
+                        viewMode: 1,
                         crop(event) {
                             canvas = this.cropper.getCroppedCanvas();
                             croppedImg.src = canvas.toDataURL("image/png");
                         },
                     });
                 }, 1)
-                
             }
+            this.blockInputs = true;
         },
 
         setWidth(event) {
-            if (this.startWidth >= 10 && this.startWidth <= 600) {
-                if (this.cropMode) {
-                    const croppedImg = this.$refs.cropBaget;
-                    // потом подставлять динамическое значение
-                    this.borderWidth = 4;
-                    this.bagetHeight = Math.round(this.croppedImgHeight * this.startWidth / this.croppedImgWidth);
-                    this.startHeight = Math.round(this.bagetHeight);
-                    this.bagetWidth = this.startWidth;
-                    this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                    this.emptyMode = true;
-                    this.heightScale = this.scale(this.bagetHeight, 400, 'height');
-                    this.widthScale = this.scale(this.bagetWidth, 600, 'width');
-                    this.bagetWidth = this.sideScale(this.bagetWidth, Math.min(this.heightScale, this.widthScale));
-                    this.bagetHeight = this.sideScale(this.bagetHeight, Math.min(this.heightScale, this.widthScale));
-                    this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                    this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
-                    croppedImg.height = this.bagetHeight - 2 * this.borderWidth + 2;
-                    croppedImg.width = this.bagetWidth - 2 * this.borderWidth + 2;
-                    croppedImg.src = this.croppedImgSrc;
-                } else {
-                    if (this.startWidth === '' && this.startHeight === '') {
-                        this.emptyMode = false;
-                    } else {
-                        this.emptyMode = true;
+            if (!this.blockInputs) {
+                if (this.startWidth >= 10 && this.startWidth <= 600) {
+                    if (this.cropMode) {
+                        const croppedImg = this.$refs.cropBaget;
                         // потом подставлять динамическое значение
                         this.borderWidth = 4;
-                        this.startWidth = +event.target.value;
-                        this.widthScale = this.scale(+this.startWidth, 600, 'width');
-                        if (this.heightScale > 1 && this.widthScale > 1) {
-                            this.bagetWidth = this.sideScale(+this.startWidth, Math.min(this.heightScale, this.widthScale));
-                            this.bagetHeight = this.sideScale(+this.startHeight, Math.min(this.heightScale, this.widthScale));
-                            this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                            this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
-                        } else if (this.bagetHeight > 0){
-                            this.bagetWidth = this.startWidth;
-                            this.bagetHeight = this.startHeight;
-                            this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.bagetHeight = Math.round(this.croppedImgHeight * this.startWidth / this.croppedImgWidth);
+                        this.startHeight = Math.round(this.bagetHeight);
+                        this.bagetWidth = this.startWidth;
+                        this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.emptyMode = true;
+                        this.heightScale = this.scale(this.bagetHeight, 400, 'height');
+                        this.widthScale = this.scale(this.bagetWidth, 600, 'width');
+                        this.bagetWidth = this.sideScale(this.bagetWidth, Math.min(this.heightScale, this.widthScale));
+                        this.bagetHeight = this.sideScale(this.bagetHeight, Math.min(this.heightScale, this.widthScale));
+                        this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
+                        croppedImg.height = this.bagetHeight - 2 * this.borderWidth + 2;
+                        croppedImg.width = this.bagetWidth - 2 * this.borderWidth + 2;
+                        croppedImg.src = this.croppedImgSrc;
+                    } else {
+                        if (this.startWidth === '' && this.startHeight === '') {
+                            this.emptyMode = false;
+                        } else {
+                            this.emptyMode = true;
+                            // потом подставлять динамическое значение
+                            this.borderWidth = 4;
+                            this.startWidth = +event.target.value;
+                            this.widthScale = this.scale(+this.startWidth, 600, 'width');
+                            if (this.heightScale > 1 && this.widthScale > 1) {
+                                this.bagetWidth = this.sideScale(+this.startWidth, Math.min(this.heightScale, this.widthScale));
+                                this.bagetHeight = this.sideScale(+this.startHeight, Math.min(this.heightScale, this.widthScale));
+                                this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                                this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
+                            } else if (this.bagetHeight > 0){
+                                this.bagetWidth = this.startWidth;
+                                this.bagetHeight = this.startHeight;
+                                this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                            }
                         }
                     }
                 }
+            } else {
+                this.startWidth = '';
             }
         },
 
         setHeight(event) {
-            if (this.startHeight >= 10 && this.startHeight <= 400) {
-                if (this.cropMode) {
-                    const croppedImg = this.$refs.cropBaget;
-                    // потом подставлять динамическое значение
-                    this.borderWidth = 4;
-                    this.bagetWidth = Math.round(this.croppedImgWidth * this.startHeight / this.croppedImgHeight);
-                    this.startWidth = Math.round(this.bagetWidth);
-                    this.bagetHeight = this.startHeight;
-                    this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                    this.emptyMode = true;
-                    this.heightScale = this.scale(this.bagetHeight, 400, 'height');
-                    this.widthScale = this.scale(this.bagetWidth, 600, 'width');
-                    this.bagetWidth = this.sideScale(this.bagetWidth, Math.min(this.heightScale, this.widthScale));
-                    this.bagetHeight = this.sideScale(this.bagetHeight, Math.min(this.heightScale, this.widthScale));
-                    this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                    this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
-                    croppedImg.height = this.bagetHeight - (2 * this.borderWidth) + 2;
-                    croppedImg.width = this.bagetWidth - (2 * this.borderWidth) + 2;
-                    croppedImg.src = this.croppedImgSrc;
-                    this.$forceUpdate();
-                } else {
-                    if (this.startWidth === '' && this.startHeight === '') {
-                        this.emptyMode = false;
-                    } else {
-                        this.emptyMode = true;
+            if (!this.blockInputs) {
+                if (this.startHeight >= 10 && this.startHeight <= 400) {
+                    if (this.cropMode) {
+                        const croppedImg = this.$refs.cropBaget;
                         // потом подставлять динамическое значение
                         this.borderWidth = 4;
-                        this.startHeight = +event.target.value;
-                        this.heightScale = this.scale(+this.startHeight, 400, 'height');
-                        if (this.widthScale > 1 && this.heightScale > 1) {
-                            this.bagetWidth = this.sideScale(+this.startWidth, Math.min(this.heightScale, this.widthScale));
-                            this.bagetHeight = this.sideScale(+this.startHeight, Math.min(this.heightScale, this.widthScale));
-                            this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
-                            this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
-                        } else  if (this.bagetWidth > 0) {
-                            this.bagetWidth = this.startWidth;
-                            this.bagetHeight = this.startHeight;
-                            this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.bagetWidth = Math.round(this.croppedImgWidth * this.startHeight / this.croppedImgHeight);
+                        this.startWidth = Math.round(this.bagetWidth);
+                        this.bagetHeight = this.startHeight;
+                        this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.emptyMode = true;
+                        this.heightScale = this.scale(this.bagetHeight, 400, 'height');
+                        this.widthScale = this.scale(this.bagetWidth, 600, 'width');
+                        this.bagetWidth = this.sideScale(this.bagetWidth, Math.min(this.heightScale, this.widthScale));
+                        this.bagetHeight = this.sideScale(this.bagetHeight, Math.min(this.heightScale, this.widthScale));
+                        this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                        this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
+                        croppedImg.height = this.bagetHeight - (2 * this.borderWidth) + 2;
+                        croppedImg.width = this.bagetWidth - (2 * this.borderWidth) + 2;
+                        croppedImg.src = this.croppedImgSrc;
+                        this.$forceUpdate();
+                    } else {
+                        if (this.startWidth === '' && this.startHeight === '') {
+                            this.emptyMode = false;
+                        } else {
+                            this.emptyMode = true;
+                            // потом подставлять динамическое значение
+                            this.borderWidth = 4;
+                            this.startHeight = +event.target.value;
+                            this.heightScale = this.scale(+this.startHeight, 400, 'height');
+                            if (this.widthScale > 1 && this.heightScale > 1) {
+                                this.bagetWidth = this.sideScale(+this.startWidth, Math.min(this.heightScale, this.widthScale));
+                                this.bagetHeight = this.sideScale(+this.startHeight, Math.min(this.heightScale, this.widthScale));
+                                this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                                this.borderWidth = this.sideScale(this.borderWidth, Math.min(this.heightScale, this.widthScale));
+                            } else  if (this.bagetWidth > 0) {
+                                this.bagetWidth = this.startWidth;
+                                this.bagetHeight = this.startHeight;
+                                this.sideContainerWidth = Math.sqrt((this.bagetHeight * this.bagetHeight) / 2);
+                            }
                         }
                     }
                 }
+            } else {
+                this.startHeight = '';
             }
         },
 
@@ -164,8 +173,13 @@ const app = new Vue({
             const croppedImg = this.$refs.croppedImg;
             this.croppedImgHeight = croppedImg.height;
             this.croppedImgWidth = croppedImg.width;
+            if (this.croppedImgHeight > this.croppedImgWidth) {
+                this.croppedImgHeight = 400;
+                this.croppedImgWidth = this.croppedImgWidth * 400 / croppedImg.height;
+            }
             this.croppedImgSrc = croppedImg.src;
             this.cropMode = true;
+            this.blockInputs = false;
         }
     },
     mounted() {
